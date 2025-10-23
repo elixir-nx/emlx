495d57e6d920a48fb5b7eacadc23879e01ab7a9d
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2025-10-22 23:01:23 -0300

    fix: disable compiler mode until nif_call fix" (#89)
    * fix: disable compiler mode until nif_call fix
    
    * fix: remove nif call runner from app tree

--- lib/emlx/application.ex
+++ lib/emlx/application.ex
@@ -2,9 +2,7 @@ defmodule EMLX.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      {NifCall.Runner, runner_opts: [nif_module: EMLX.NIF], name: EMLX.Runner}
-    ]
+    children = []
 
     Supervisor.start_link(children, strategy: :one_for_one, name: EMLX.Supervisor)
   end
