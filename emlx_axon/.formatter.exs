7f78ce2b61bf95cdd1d28e4a07b8475ca5e9ff05
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2026-05-08 17:56:12 -0300

    formatter

--- emlx_axon/.formatter.exs
+++ emlx_axon/.formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   import_deps: [:nx],
-  locals_without_parens: [deftensor: 1, defdevice: 1, defvalue: 1],
+  locals_without_parens: [],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
