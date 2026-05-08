19464e5021f6a59850420056241a360b01a75837
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2026-05-08 17:54:10 -0300

    fix nifs

--- c_src/nx_nif_utils.hpp
+++ c_src/nx_nif_utils.hpp
@@ -120,41 +120,41 @@ namespace nif {
 // Status helpers
 
 // Helper for returning `{:error, msg}` from NIF.
-ERL_NIF_TERM error(ErlNifEnv *env, const char *msg) {
+inline ERL_NIF_TERM error(ErlNifEnv *env, const char *msg) {
   ERL_NIF_TERM atom = enif_make_atom(env, "error");
   ERL_NIF_TERM msg_term = enif_make_string(env, msg, ERL_NIF_LATIN1);
   return enif_make_tuple2(env, atom, msg_term);
 }
 
 // Helper for returning `{:ok, term}` from NIF.
-ERL_NIF_TERM ok(ErlNifEnv *env) { return enif_make_atom(env, "ok"); }
+inline ERL_NIF_TERM ok(ErlNifEnv *env) { return enif_make_atom(env, "ok"); }
 
 // Helper for returning `:ok` from NIF.
-ERL_NIF_TERM ok(ErlNifEnv *env, ERL_NIF_TERM term) {
+inline ERL_NIF_TERM ok(ErlNifEnv *env, ERL_NIF_TERM term) {
   return enif_make_tuple2(env, ok(env), term);
 }
 
 // Numeric types
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, int *var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, int *var) {
   return enif_get_int(env, term, reinterpret_cast<int *>(var));
 }
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, int64_t *var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, int64_t *var) {
   return enif_get_int64(env, term, reinterpret_cast<ErlNifSInt64 *>(var));
 }
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, size_t *var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, size_t *var) {
   return enif_get_uint64(env, term, reinterpret_cast<ErlNifUInt64 *>(var));
 }
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, double *var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, double *var) {
   return enif_get_double(env, term, var);
 }
 
 // Standard types
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
   unsigned len;
   int ret = enif_get_list_length(env, term, &len);
 
@@ -181,41 +181,41 @@ int get(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
   return ret;
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, bool var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, bool var) {
   if (var)
     return enif_make_atom(env, "true");
 
   return enif_make_atom(env, "false");
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, int64_t var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, int64_t var) {
   return enif_make_int64(env, var);
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, size_t var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, size_t var) {
   return enif_make_uint64(env, var);
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, int var) { return enif_make_int(env, var); }
+inline ERL_NIF_TERM make(ErlNifEnv *env, int var) { return enif_make_int(env, var); }
 
-ERL_NIF_TERM make(ErlNifEnv *env, double var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, double var) {
   return enif_make_double(env, var);
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, ErlNifBinary var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, ErlNifBinary var) {
   return enif_make_binary(env, &var);
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, std::string var) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, std::string var) {
   return enif_make_string(env, var.c_str(), ERL_NIF_LATIN1);
 }
 
-ERL_NIF_TERM make(ErlNifEnv *env, const char *string) {
+inline ERL_NIF_TERM make(ErlNifEnv *env, const char *string) {
   return enif_make_string(env, string, ERL_NIF_LATIN1);
 }
 
 template <typename T>
-ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<T> result) {
+inline ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<T> result) {
   size_t n = result.size();
 
   std::vector<ERL_NIF_TERM> nif_terms;
@@ -230,7 +230,7 @@ ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<T> result) {
   return list;
 }
 
-ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<mlx::core::array> result) {
+inline ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<mlx::core::array> result) {
   size_t n = result.size();
 
   std::vector<ERL_NIF_TERM> nif_terms;
@@ -247,7 +247,7 @@ ERL_NIF_TERM make_list(ErlNifEnv *env, std::vector<mlx::core::array> result) {
 
 // Atoms
 
-int get_atom(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
+inline int get_atom(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
   unsigned atom_length;
   if (!enif_get_atom_length(env, term, &atom_length, ERL_NIF_LATIN1)) {
     return 0;
@@ -263,13 +263,13 @@ int get_atom(ErlNifEnv *env, ERL_NIF_TERM term, std::string &var) {
   return 1;
 }
 
-ERL_NIF_TERM atom(ErlNifEnv *env, const char *msg) {
+inline ERL_NIF_TERM atom(ErlNifEnv *env, const char *msg) {
   return enif_make_atom(env, msg);
 }
 
 // Boolean
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, bool *var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, bool *var) {
   std::string bool_atom;
   if (!get_atom(env, term, bool_atom))
     return 0;
@@ -286,7 +286,7 @@ int get(ErlNifEnv *env, ERL_NIF_TERM term, bool *var) {
 
 // function
 
-int get(ErlNifEnv *env, ERL_NIF_TERM term, emlx::function *&var) {
+inline int get(ErlNifEnv *env, ERL_NIF_TERM term, emlx::function *&var) {
   return enif_get_resource(env, term, resource_object<emlx::function>::type,
                            reinterpret_cast<void **>(&var));
 }
@@ -294,7 +294,7 @@ int get(ErlNifEnv *env, ERL_NIF_TERM term, emlx::function *&var) {
 // Containers
 
 template <typename T = int64_t>
-int get_tuple(ErlNifEnv *env, ERL_NIF_TERM tuple, std::vector<T> &var) {
+inline int get_tuple(ErlNifEnv *env, ERL_NIF_TERM tuple, std::vector<T> &var) {
   const ERL_NIF_TERM *terms;
   int length;
   if (!enif_get_tuple(env, tuple, &length, &terms))
@@ -310,7 +310,7 @@ int get_tuple(ErlNifEnv *env, ERL_NIF_TERM tuple, std::vector<T> &var) {
   return 1;
 }
 
-int get_list(ErlNifEnv *env, ERL_NIF_TERM list,
+inline int get_list(ErlNifEnv *env, ERL_NIF_TERM list,
              std::vector<mlx::core::array> &var) {
   unsigned int length;
   if (!enif_get_list_length(env, list, &length))
@@ -330,7 +330,7 @@ int get_list(ErlNifEnv *env, ERL_NIF_TERM list,
   return 1;
 }
 
-int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<std::string> &var) {
+inline int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<std::string> &var) {
   unsigned int length;
   if (!enif_get_list_length(env, list, &length))
     return 0;
@@ -347,7 +347,7 @@ int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<std::string> &var) {
   return 1;
 }
 
-int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<int> &var) {
+inline int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<int> &var) {
   unsigned int length;
   if (!enif_get_list_length(env, list, &length))
     return 0;
@@ -364,7 +364,7 @@ int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<int> &var) {
   return 1;
 }
 
-int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<size_t> &var) {
+inline int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<size_t> &var) {
   unsigned int length;
   if (!enif_get_list_length(env, list, &length))
     return 0;
@@ -381,7 +381,7 @@ int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<size_t> &var) {
   return 1;
 }
 
-int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<int64_t> &var) {
+inline int get_list(ErlNifEnv *env, ERL_NIF_TERM list, std::vector<int64_t> &var) {
   unsigned int length;
   if (!enif_get_list_length(env, list, &length))
     return 0;
